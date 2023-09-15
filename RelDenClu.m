ae6d0654bce665c468e55cabca7d4c7bba0a8f9b
namitaML <121678503+namitaML@users.noreply.github.com>
2023-09-15 20:04:07 +0530

    Add files via upload

--- RelDenClu.m
+++ RelDenClu.m
@@ -1,5 +1,26 @@
-function [clubase, cludim]=RelDenClu(d, ObsInMinBase, maxSingleLinkageFeat)
-d=norm1(d);
-[ixmat]=preproFast(d);
-[impbase, impdim]=bicRMfunc(ixmat, ObsInMinBase );
-[clubase, cludim]=getbiclus(impbase,d, maxSingleLinkageFeat);
+function [clubase, cludim]=RelDenClu(d)    
+    [n,m]=size(d);
+    d=(d-repmat(min(d),n,1))./repmat(range(d), n,1);
+
+    [ixmat]=preproFast(d);
+    temp1=sum(ixmat>0,1);
+    histn=ceil(3*log(m*(m-1)/2)/log(2));
+    histedges=0:n/histn:n;
+    hyp=histcounts(temp1(:),histedges );
+    hyp(1)=[];
+    for k=1:(histn-1)
+        if(hyp(k)>0 & hyp(k+1)>0) break;
+        end;
+    end;
+
+    obsmin=ceil(histedges(k));
+    [impbase, impdim]=bicRMfunc(ixmat,obsmin );
+    
+    szimp=size(impbase,1);
+    if(isempty(impbase)||szimp<2)
+        clubase=zeros(1,n);
+        cludim=zeros(1,m);  
+    else
+    noclu=ceil(obsmin/size(impdim,1))+3;
+    [clubase, cludim]=getbiclus(impbase, d, noclu);
+    end;
